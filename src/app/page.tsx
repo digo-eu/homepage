0adce4860e47c30fcfdb769e98620ca4af94ec80
digo-eu <roz.andrade@gmail.com>
2024-05-23 08:19:50 +0000

    Placeholder visual

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,27 @@
 import Link from "next/link";
 
 export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<main class="min-h-screen bg-gradient-to-b from-[#204b60] to-[#1b1e2d] text-white">
+		<nav data-orientation="horizontal" dir="ltr" class="flex gap-12 px-4 py-4">
+		<span class="container flex text-4xl font-extrabold tracking-tight text-white sm:text-[3rem]">Rodrigo Andrade</span>
+		<ul data-orientation="horizontal" dir="rtl" class="container flex gap-12">
+		<li><a class="text-2xl font-bold" href="/casamento">Casamento</a></li>
+		<li><a class="text-2xl font-bold" href="/air">Go+HTMX</a></li>
+		<li><a class="text-2xl font-bold" href="/geoserver">Geoserver</a></li>
+		</ul>
+		<svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" viewBox="0 0 24 40" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+			<line x1="4" x2="20" y1="6" y2="6"></line>
+			<line x1="4" x2="20" y1="12" y2="12"></line>
+		<line x1="4" x2="20" y1="18" y2="18"></line>
+		</svg>
+		</nav>
+		<div class="w-100">
+		<img src="https://utfs.io/f/648b28b1-0df2-40e0-8854-c753d60edfdd-1l7cp.png" alt="Vlad is a cat" />
+			</div>
+		<div class="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+		Hello world
+		</div>
+		</main>
+	);
 }
