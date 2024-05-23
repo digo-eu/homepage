cf34ff32d7b451ab9d52e67d5e51ce6cbe611777
digo-eu <roz.andrade@gmail.com>
2024-05-23 09:11:30 +0000

    Added top nav bar to default layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <body>
+    <nav data-orientation="horizontal" dir="ltr" className="w-full sticky top-0 flex gap-12 px-4 py-4">
+    <span className="container flex text-4xl font-extrabold tracking-tight text-white sm:text-[3rem]">Rodrigo Andrade</span>
+    <ul data-orientation="horizontal" dir="rtl" className="container flex gap-12">
+    <li><a className="text-2xl font-bold" href="/casamento">Casamento</a></li>
+    <li><a className="text-2xl font-bold" href="/air">Go+HTMX</a></li>
+    <li><a className="text-2xl font-bold" href="/geoserver">Geoserver</a></li>
+    </ul>
+    <svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" viewBox="0 0 24 40" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+	    <line x1="4" x2="20" y1="6" y2="6"></line>
+    <line x1="4" x2="20" y1="12" y2="12"></line>
+    <line x1="4" x2="20" y1="18" y2="18"></line>
+    </svg>
+    </nav>
+
+    {children}
+
+    </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,7 @@ import Link from "next/link";
 
 export default function HomePage() {
 	return (
-		<main className="min-h-screen bg-gradient-to-b from-[#204b60] to-[#1b1e2d] text-white">
-		<nav data-orientation="horizontal" dir="ltr" className="flex gap-12 px-4 py-4">
-		<span className="container flex text-4xl font-extrabold tracking-tight text-white sm:text-[3rem]">Rodrigo Andrade</span>
-		<ul data-orientation="horizontal" dir="rtl" className="container flex gap-12">
-		<li><a className="text-2xl font-bold" href="/casamento">Casamento</a></li>
-		<li><a className="text-2xl font-bold" href="/air">Go+HTMX</a></li>
-		<li><a className="text-2xl font-bold" href="/geoserver">Geoserver</a></li>
-		</ul>
-		<svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" viewBox="0 0 24 40" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-			<line x1="4" x2="20" y1="6" y2="6"></line>
-			<line x1="4" x2="20" y1="12" y2="12"></line>
-		<line x1="4" x2="20" y1="18" y2="18"></line>
-		</svg>
-		</nav>
+		<main className="min-h-screen">
 		<div className="w-100">
 		<img src="https://utfs.io/f/648b28b1-0df2-40e0-8854-c753d60edfdd-1l7cp.png" alt="Vlad is a cat" />
 			</div>
