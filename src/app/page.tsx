00db2e0acabd6fc14f24b3e2059773578b54668c
digo-eu <roz.andrade@gmail.com>
2024-05-23 08:32:52 +0000

    Swapped 'class' for 'className' due to compilation errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export default function HomePage() {
 	return (
-		<main class="min-h-screen bg-gradient-to-b from-[#204b60] to-[#1b1e2d] text-white">
-		<nav data-orientation="horizontal" dir="ltr" class="flex gap-12 px-4 py-4">
-		<span class="container flex text-4xl font-extrabold tracking-tight text-white sm:text-[3rem]">Rodrigo Andrade</span>
-		<ul data-orientation="horizontal" dir="rtl" class="container flex gap-12">
-		<li><a class="text-2xl font-bold" href="/casamento">Casamento</a></li>
-		<li><a class="text-2xl font-bold" href="/air">Go+HTMX</a></li>
-		<li><a class="text-2xl font-bold" href="/geoserver">Geoserver</a></li>
+		<main className="min-h-screen bg-gradient-to-b from-[#204b60] to-[#1b1e2d] text-white">
+		<nav data-orientation="horizontal" dir="ltr" className="flex gap-12 px-4 py-4">
+		<span className="container flex text-4xl font-extrabold tracking-tight text-white sm:text-[3rem]">Rodrigo Andrade</span>
+		<ul data-orientation="horizontal" dir="rtl" className="container flex gap-12">
+		<li><a className="text-2xl font-bold" href="/casamento">Casamento</a></li>
+		<li><a className="text-2xl font-bold" href="/air">Go+HTMX</a></li>
+		<li><a className="text-2xl font-bold" href="/geoserver">Geoserver</a></li>
 		</ul>
 		<svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" viewBox="0 0 24 40" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 			<line x1="4" x2="20" y1="6" y2="6"></line>
@@ -16,10 +16,10 @@ export default function HomePage() {
 		<line x1="4" x2="20" y1="18" y2="18"></line>
 		</svg>
 		</nav>
-		<div class="w-100">
+		<div className="w-100">
 		<img src="https://utfs.io/f/648b28b1-0df2-40e0-8854-c753d60edfdd-1l7cp.png" alt="Vlad is a cat" />
 			</div>
-		<div class="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+		<div className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
 		Hello world
 		</div>
 		</main>
