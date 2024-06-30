d9dfcf4145feed48591132510f4f62828697b516
digo-eu <roz.andrade@gmail.com>
2024-06-30 12:09:14 -0300

    Content and styling for wedding page

---
+++ src/app/casamento/layout.tsx
@@ -0,0 +1,7 @@
+export default function Layout({children,}: {children: any;}) {
+    return (
+        <div className="bg-50% bg-contain bg-right-top bg-no-repeat bg-[url('https://utfs.io/f/811b2648-2fac-4b12-a6cb-a3e36876ba60-e0n5iu.webp')]">
+            {children}
+        </div>
+    );
+}
--- src/app/casamento/page.tsx
+++ src/app/casamento/page.tsx
@@ -39,13 +39,40 @@ async function Images() {
 export default function CasamentoPage() {
 	return (
 		<main className="">
-            <p className="text-xl p-4">Bem vindo!</p>
-            <p className="p-4">
-                Aqui contamos um pouquinho dessa história que todos acompanharam, mas que podem não ter visto por inteiro.
-            </p>
-            <p className="p-4">
-                Por muitos anos estudamos na mesma escola, mas em séries diferentes. Nos víamos nos corredores...
-            </p>
+			<div className="p-4">
+					<h1 className="text-4xl">Bem vindo!</h1>
+					<br></br>
+					<h2 className="text-2xl font-bold">Como confirmar presença?</h2>
+					<p>Através do Whatsapp de um dos noivos:</p>
+					<p>Taís: (11) 96370-5024</p>
+					<p>Rodrigo: (11) 99243-5070</p>
+					<p>Pedimos para que seja confirmado presença o quanto antes possível, pois essa informação será repassada aos fornecedores do evento.</p>
+					<br></br>
+					<h2 className="text-2xl font-bold">O que saber sobre o dia?</h2>
+					<p>O casamento será dia 20 de Novembro de 2024. Uma quarta-feira, feriado nacional. Escolhemos essa data por ser especial para nós: completaremos 10 anos juntos.</p>
+					<p>Não pretendemos fazer cerimônia, e sim recepção, que será um café colonial. Portanto, é só chegar, se servir à vontade e se divertir!</p>
+					<br></br>
+					<h2 className="text-2xl font-bold">Sobre o casal:</h2>
+					<p>Por muitos anos estudamos na mesma escola, mas em séries diferentes. Nos víamos nos corredores, sabíamos da existência do outro, seguíamos um ao outro em redes sociais (Facebook e Instagram). Mas, até sairmos desta escola para nos prepararmos para o vestibular, nunca nos falamos.</p>
+					<p>Em outubro de 2014, Rodrigo, vendo que os perfis de redes sociais da Taís possuíam muitos desenhos dela, resolveu puxar assunto sobre isso.</p>
+					<p>[De repente, colocar o quadro de Campos do Jordão aqui]</p>
+					<p>Aproveitaram para conversar pessoalmente também, e em pouquíssimo tempo descobriram coisas essenciais em comum:</p>
+					<p>Amor pelas artes. Enquanto Taís era desenhista, Rodrigo escrevia poesias.</p>
+					<p>[Colocar a poesia – ou trechos dela – que o meu amor escreveu pra mim]</p>
+					<br></br>
+					<p>Amor por animais. Ambos conviveram com muitos bichinhos no decorrer da vida, e cultivaram com isso um amor infinito por cada um deles (6 cachorros e 25 gatos da Taís; 1 coelho, 2 cachorros e 10 gatos do Rodrigo). Hoje, moram com 2 gatinhos lindos, Pinta e Vlad.</p>
+					<p>[FOTO DOS GATINHOS LINDOS]</p>
+					<br></br>
+					<p>6 cachorros e 25 gatos: Godinha, Lepinha, Dollynha, Lila, Tammy, Kell, Fifo, Bóris, Zazá, Branco, Lili, Bibi, Zilu, Bóris Júnior, Bóris Chorão, Pelé, Fuka, Filó, Laila, Peter, Safi, Sininho, Negrinho, Mia, Jack, Toulouse, Luna, Tommen, Nala, Maggie e Quin.</p>
+					<p>2 cachorros, 1 coelho e 10 gatos: Tico Puccini e Teco Vivaldi, Floquinho, Pepe, Elvis, Felpudo, Ozzy, Princesa, Ágata, Pinta, Nina, Vlad, Frajolinha.</p>
+					<br></br>
+					<p>Amor por passear em lugares bonitos. Conhecer lugares, andar no meio da natureza, ver bichinhos.</p>
+					<p>[Fotos de nós dois em umas paisagens fofinhas)</p>
+					<br></br>
+					<p>Amor por jogos (de computador, videogame).</p>
+					<p>[Pode botar fotos da nossa casinha do Minecraft – e outras)</p>
+					<br></br>
+			</div>
 			
 			<Images />
 
